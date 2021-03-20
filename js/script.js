8d6ed45f80f4de8f3c00116577f6e7aa7117b0c3
Eunit <eunithelp@gmail.com>
2021-03-20 15:52:53 +0100

    Validation 80% setup

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const repeatedPassword = document.getElementById("repeated-password");
 
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInputs();
@@ -31,16 +32,36 @@ function checkInputs() {
   const passwordValue = password.value.trim();
   const repeatedPasswordValue = repeatedPassword.value.trim();
 
-  if (usernameValue === "") {
-    // show some error
 
+  if (usernameValue === "") {
     setErrorFor(username, "Username cannot be blank");
-    // add error class
   } else {
-    // add success class
     setSuccesFor(username)
   }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  } else {
+    setSuccesFor(email)
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else {
+    setSuccesFor(password)
+  }
+
+  if (repeatedPasswordValue === "") {
+    setErrorFor(repeatedPasswordValue, "Repeated password cannot be blank");
+  } else if (repeatedPasswordValue !== passwordValue) {
+    setErrorFor(repeatedPasswordValue, "Repeated password does not match");
+  } else {
+    setSuccesFor(repeatedPasswordValue)
+  }
 }
+
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement; //.form-control
@@ -49,5 +70,14 @@ function setErrorFor(input, message) {
 
   // add error class
   formControl.className = "form-control error";
-  alert("Error");
+  console.log(message);
+}
+
+function setErrorFor(input) {
+  const formControl = input.parentElement; //.form-control
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
